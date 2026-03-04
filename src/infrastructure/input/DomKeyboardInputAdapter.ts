44744bb1a4ca885cc03e1754e05556af9db49811
AzureAD\cristianrodriguez <cristian.rodriguez@cuatroochenta.com>
2026-03-04 09:48:23 +0100

    Chore: add DomKeyboardInputAdapter.ts hear the keybord events of the DOM

---
+++ src/infrastructure/input/DomKeyboardInputAdapter.ts
@@ -0,0 +1,13 @@
+import { InputHandler, InputSourcePort } from "../../application/ports/InputSourcePort";
+
+export class DomKeyboardInputAdapter implements InputSourcePort {
+
+    suscribe(handler: InputHandler): void {
+        
+        suscribe (handler: InputHandler): void {
+
+        Array.form(document)(document.getElementsByClassName("key")).array.forEach(element => {
+            
+        });
+    }
+}
