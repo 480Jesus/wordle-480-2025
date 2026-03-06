5a726e7f1f906401c84306ba6f4c2484ef2f7b52
AzureAD\cristianrodriguez <cristian.rodriguez@cuatroochenta.com>
2026-03-06 10:32:31 +0100

    style: delete comments

--- src/infrastructure/input/DomKeyboardInputAdapter.ts
+++ src/infrastructure/input/DomKeyboardInputAdapter.ts
@@ -3,18 +3,15 @@ import { InputHandler, InputSourcePort } from "../../application/ports/InputSour
 export class DomKeyboardInputAdapter implements InputSourcePort {
     subscribe(handler: InputHandler): void {
 
-        // Teclado visual (botones en pantalla)
         Array.from(document.getElementsByClassName("key")).forEach((element) => {
             element.addEventListener("click", (e: Event) => {
-                e.preventDefault(); // ✅ Evita que el click dispare también un keydown
+                e.preventDefault();
                 const button = e.currentTarget as HTMLButtonElement;
                 handler(button.value);
             });
         });
 
-        // Teclado físico
         document.addEventListener("keydown", (e: KeyboardEvent) => {
-            // ✅ Evita que Enter físico se ejecute dos veces si el foco está en un botón
             if (e.code === "Enter" && (e.target as HTMLElement).tagName === "BUTTON") {
                 e.preventDefault();
                 handler("Enter");
